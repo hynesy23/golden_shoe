6cd151041f4d2b1c9977359845156df2c35d15ec
DESKTOP-NG4HA27\hynes <cillian.hynes@lightning-e.com>
2021-06-27 23:54:05 +0100

    Adds react-router-dom to add routing to components

--- src/App.js
+++ src/App.js
@@ -5,11 +5,20 @@ import { Header } from './components/Header';
 import { Homepage } from './components/Homepage';
 import { Footer } from './components/Footer';
 
+import { Route, Switch } from 'react-router-dom';
+import { CollectionsPage } from './components/CollectionsPage';
+import { ProductPage } from './components/ProductPage';
+
 function App() {
   return (
     <div className="App">
       <Header />
-      <Homepage />
+      <Switch>
+        {/* <Homepage /> */}
+        <Route path="/" component={Homepage} exact />
+        <Route path="/:collection" component={CollectionsPage} exact />
+        <Route path="/collections/:product-name" component={ProductPage} exact />
+      </Switch>
       <Footer />
     </div>
   );
